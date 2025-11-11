f25534d461a3d33cdf9b1066b5bc0cc69ad7e815
Bill <khoibtvn03@gmail.com>
2025-11-11 07:29:38 +0700

    validation POST semester api

--- FGS-BE.Services/Services/SemesterService.cs
+++ FGS-BE.Services/Services/SemesterService.cs
@@ -5,6 +5,7 @@ using FGS_BE.Repo.Repositories.Implements;
 using FGS_BE.Repo.Repositories.Interfaces;
 using FGS_BE.Services.Interfaces;
 using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace FGS_BE.Services.Services
 {
@@ -50,11 +51,33 @@ namespace FGS_BE.Services.Services
 
         public async Task<SemesterDto> CreateAsync(CreateSemesterDto dto)
         {
+            if (dto.EndDate <= dto.StartDate)
+            {
+                throw new Exception("Ngày kết thúc không thể nhỏ hơn hoặc bằng ngày bắt đầu.");
+            }
+
+            var overlappingSemester = await _unitOfWork.SemesterRepository.Entities
+                .AsNoTracking()
+                .Where(x =>
+                    (dto.StartDate >= x.StartDate && dto.StartDate <= x.EndDate) || 
+                    (dto.EndDate >= x.StartDate && dto.EndDate <= x.EndDate) ||  
+                    (dto.StartDate <= x.StartDate && dto.EndDate >= x.EndDate))     
+                .FirstOrDefaultAsync();
+
+            if (overlappingSemester != null)
+            {
+                throw new Exception("Thời gian của học kỳ bị trùng với một học kỳ đã tồn tại.");
+            }
+
             var entity = dto.ToEntity();
+
             await _unitOfWork.SemesterRepository.CreateAsync(entity);
             await _unitOfWork.CommitAsync();
+
             return new SemesterDto(entity);
         }
+
+
 
         public async Task<SemesterDto?> UpdateAsync(int id, UpdateSemesterDto dto)
         {
